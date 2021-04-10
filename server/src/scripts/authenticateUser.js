c761aa6469684b4152a74054a2a72b75c3a975c5
Russell Shurts <rshurts@users.noreply.github.com>
2021-04-10 18:42:19 -0500

    Add seed data

--- server/src/scripts/authenticateUser.js
+++ server/src/scripts/authenticateUser.js
@@ -1,11 +1,10 @@
 import getToken from "../lib/getToken";
 
 (async () => {
-  // const [email, password] = process.argv.slice(2);
-  const access_token = await getToken(
-    process.env.EMAIL,
-    process.env.PASSWORD
-  ).catch((error) => {
+  const [emailArg, passwordArg] = process.argv.slice(2);
+  const email = emailArg || process.env.EMAIL;
+  const password = passwordArg || process.env.PASSWORD;
+  const access_token = await getToken(email, password).catch((error) => {
     console.error(error);
   });
   console.log(access_token);
